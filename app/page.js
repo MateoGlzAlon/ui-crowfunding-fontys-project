67417eab736cb7bd976408a30e290cc76dad1b6a
MateoGlzAlon <mateo.gonzalez.alonso@gmail.com>
2024-10-30 13:20:29 +0100

    try bugfix

--- app/page.js
+++ app/page.js
@@ -12,19 +12,21 @@ export default function Home() {
 
   const router = useRouter()
 
+
+
   return (
     <main>
 
       <PageFrame>
-
+        {console.log(1)}
         <DotPatternAndTitle />
-
+        {console.log(2)}
         <HighlightedProjectsList />
-
+        {console.log(3)}
         <NewProjectsList />
-
+        {console.log(4)}
       </PageFrame>
-
+      {console.log(5)}
     </main>
 
 
