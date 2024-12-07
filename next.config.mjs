5fff120775aa44df454add909efddc449df4496c
MateoGlzAlon <mateo.gonzalez.alonso@gmail.com>
2024-12-08 00:46:34 +0100

    added Dockerization to pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //reactStrictMode: false, // Set to 'false' to disable React Strict Mode
+    output: "standalone",
 };
 
 export default nextConfig;
