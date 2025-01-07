2d10851f412a3aa440c966ad4ab579f857e1e8b6
MateoGlzAlon <mateo.gonzalez.alonso@gmail.com>
2025-01-07 16:58:00 +0100

    fix

--- cypress/e2e/notloggedIn.cy.js
+++ cypress/e2e/notloggedIn.cy.js
@@ -14,7 +14,7 @@ describe('Not logged in tests', () => {
   // Test for making a payment (when not logged in)
   it('Make a payment', () => {
     cy.get('#project-card').click();
-    cy.get('#inputMoneyAmount').should('be.visible');
+    cy.get('#inputMoneyAmount', { timeout: 10000 }).should('be.visible');
     cy.get('#inputMoneyAmount').type('125');
     cy.contains('Pay').click();
     cy.contains('You need to be logged in to make a payment.');
