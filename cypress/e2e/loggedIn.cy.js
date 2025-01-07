4de94b4c62e9d2c118930ce3c3176f2f44271f1c
MateoGlzAlon <mateo.gonzalez.alonso@gmail.com>
2025-01-07 16:21:32 +0100

    e2e tests fix

--- cypress/e2e/loggedIn.cy.js
+++ cypress/e2e/loggedIn.cy.js
@@ -1,37 +1,42 @@
 describe('Logged in tests', () => {
 
+    // Function to log in to the application
+    const login = (username = 'user@example.com', password = 'pass1') => {
+        cy.visit('http://localhost:3000');
+        cy.contains('Log in').click();
+        cy.get('#username').type(username);
+        cy.get('#password').type(password);
+        cy.contains('Log In').click();
+    };
+
+    // Run the login before each test
     beforeEach(() => {
-        cy.visit('http://localhost:3000')
-        cy.contains('Log in').click()
-        cy.get('#username').click().type('user@example.com')
-        cy.get('#password').click().type('pass1')
-        cy.contains('Log In').click()
+        login();
     });
 
     it('Visit profile', () => {
-        cy.get('#user-avatar').click()
-        cy.contains('My Profile').click()
-    })
-
+        cy.get('#user-avatar').click();
+        cy.contains('My Profile').click();
+    });
 
     it('Visit a project', () => {
-        cy.get('#project-card').click()
-        cy.url().should('include', '/projects/')
-    })
-
+        cy.get('#project-card').click();
+        cy.url().should('include', '/projects/');
+    });
 
     it('Make a payment', () => {
-        cy.get('#project-card').click()
-        cy.get('#inputMoneyAmount').type('125')
-        cy.contains('Pay').click()
-        cy.contains('Confirm').click()
-    })
-
+        cy.get('#project-card').click();
+        cy.get('#inputMoneyAmount')
+            .type('125')
+            .should('have.value', '125');
+        cy.contains('Pay').click();
+        cy.contains('Confirm').click();
+    });
 
     it('Search projects', () => {
-        cy.get('#search-input').click().type('gard')
-        cy.get('#search-submit').click()
-        cy.contains('Garden')
-    })
+        cy.get('#search-input').type('gard');
+        cy.get('#search-submit').click();
+        cy.contains('Garden').should('be.visible');
+    });
 
-})
+});
--- cypress/e2e/notloggedIn.cy.js
+++ cypress/e2e/notloggedIn.cy.js
@@ -1,27 +1,28 @@
 describe('Not logged in tests', () => {
 
+  // Visiting the homepage before each test
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000');
   });
 
+  // Test for visiting a project
   it('Visit a project', () => {
-    cy.get('#project-card').click()
-    cy.url().should('include', '/projects/')
-  })
+    cy.get('#project-card').click();
+    cy.url().should('include', '/projects/');
+  });
 
-
+  // Test for making a payment (when not logged in)
   it('Make a payment', () => {
-    cy.get('#project-card').click()
-    cy.get('#inputMoneyAmount').type('125')
-    cy.contains('Pay').click()
-    cy.contains('You need to be logged in to make a payment.')
-  })
+    cy.get('#project-card').click();
+    cy.get('#inputMoneyAmount').type('125');
+    cy.contains('Pay').click();
+    cy.contains('You need to be logged in to make a payment.');
+  });
 
-
+  // Test for searching projects
   it('Search projects', () => {
-    cy.get('#search-input').click().type('gard')
-    cy.get('#search-submit').click()
-    cy.contains('Garden')
-  })
-
-})
+    cy.get('#search-input').click().type('gard');
+    cy.get('#search-submit').click();
+    cy.contains('Garden');
+  });
+});
